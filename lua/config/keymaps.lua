9664518d9ea3e50f2840879dead78b5c2624d1bd
uyaaaaaa <tosuyasocial48@gmail.com>
2024-08-09 22:30:18 +0900

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,46 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local opts = { noremap = true, silent = true }
+
+--local keymap = vim.keymap
+local keymap = vim.api.nvim_set_keymap
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- New tab
+keymap("n", "te", ":tabedit", opts)
+-- 新しいタブを一番右に作る
+keymap("n", "gn", ":tabnew<Return>", opts)
+
+-- Split window
+keymap("n", "ss", ":split<Return><C-w>w", opts)
+keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
+
+-- Do not yank with x
+keymap("n", "x", '"_x', opts)
+
+keymap("n", "<Space>h", "^", opts)
+keymap("n", "<Space>l", "$", opts)
+
+-- Insert --
+-- Press jj fast to exit insert mode
+keymap("i", "jk", "<ESC>", opts)
+
+-- コンマの後に自動的にスペースを挿入
+keymap("i", ",", ",<Space>", opts)
+
+-- Visual --
+-- 0番レジスタを使いやすくした
+keymap("v", "<C-p>", '"0p', opts)
---
+++ lua/plugins/appearance.lua
@@ -0,0 +1,13 @@
+return {
+  { "shaunsingh/nord.nvim" }, -- nord
+  { "ellisonleao/gruvbox.nvim" }, -- gruvbox
+  { "folke/tokyonight.nvim" }, -- tokyonight
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "nord",
+      -- colorscheme = "tokyonight",
+      -- colorscheme = "gruvbox",
+    },
+  },
+}
