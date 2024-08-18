ca49531fbc88eca7dafdb62ab7ceb2b1773af5c3
uyaaaaaa <tosuyasocial48@gmail.com>
2024-08-18 17:24:13 +0900

    add: toggleterm and lazygit

---
+++ nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,12 @@
+return {
+  "akinsho/toggleterm.nvim",
+  opts = {
+    open_mapping = [[<c-t>]],
+    close_on_exit = true,
+    direction = "float",
+    float_opts = {
+      border = "curved",
+      winblend = 35,
+    },
+  },
+}
