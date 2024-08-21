2870e958ee118d800a46f9ed2b710aead6430f1d
uyaaaaaa <tosuyasocial48@gmail.com>
2024-08-21 21:22:13 +0900

    remove: lazygit config

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -35,17 +35,3 @@ keymap("v", "<C-p>", '"0p', opts)
 
 -- Command --
 keymap("c", "qq", "qa", opts) -- delete all windows
-
-local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", direction = "float" })
-
-function _lazygit_toggle()
-  lazygit:toggle()
-end
-
-vim.api.nvim_set_keymap(
-  "n",
-  "<leader>gg",
-  "<cmd>lua _lazygit_toggle()<CR>",
-  { noremap = true, silent = true, desc = "Lazygit" }
-)
