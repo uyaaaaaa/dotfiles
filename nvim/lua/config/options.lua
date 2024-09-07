e6d153535e905b8380bb945a28a02c4dc94d08e3
uyaaaaaa <tosuyasocial48@gmail.com>
2024-09-07 19:01:32 +0900

    options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -9,6 +9,10 @@ opt.smartcase = true
 opt.tabstop = 4
 opt.expandtab = true
 opt.shiftwidth = 4
+opt.smartindent = true
 opt.shiftround = true
 
 opt.iskeyword:append("-")
+
+-- LSP config
+vim.g.lazyvim_php_lsp = "intelephense"
