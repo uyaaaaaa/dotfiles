815e5ad8d0901155b93282b4b11fa6b39285d714
uyaaaaaa <tosuyasocial48@gmail.com>
2024-09-07 19:00:52 +0900

    keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,41 +1,42 @@
 local opts = { noremap = true, silent = true }
 
---local keymap = vim.keymap
-local keymap = vim.api.nvim_set_keymap
+local keymap = require("vim.keymap")
 
 --Remap space as leader key
-keymap("", "<Space>", "<Nop>", opts)
+keymap.set("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Normal --
 -- window navigation
-keymap("n", "<C-h>", "<C-w>h", opts)
-keymap("n", "<C-j>", "<C-w>j", opts)
-keymap("n", "<C-k>", "<C-w>k", opts)
-keymap("n", "<C-l>", "<C-w>l", opts)
+keymap.set("n", "<C-h>", "<C-w>h", opts)
+keymap.set("n", "<C-j>", "<C-w>j", opts)
+keymap.set("n", "<C-k>", "<C-w>k", opts)
+keymap.set("n", "<C-l>", "<C-w>l", opts)
 -- yank
-keymap("n", "<S-y>", "y$", opts)
+keymap.set("n", "<S-y>", "y$", opts)
 -- Split window
-keymap("n", "ss", ":split<Return><C-w>w", opts)
-keymap("n", "sv", ":vsplit<Return><C-w>w", opts)
-keymap("n", "x", '"_x', opts) -- Do not yank with x
-keymap("n", "<Space>h", "^", opts)
-keymap("n", "<Space>l", "$", opts)
-keymap("n", "st", ":TodoTelescope<CR>", opts)
+keymap.set("n", "ss", "<Cmd>split<CR><C-w>w", opts)
+keymap.set("n", "sv", "<Cmd>vsplit<CR><C-w>w", opts)
+keymap.set("n", "x", '"_x', opts) -- Do not yank with x
+keymap.set("n", "<Space>h", "^", opts)
+keymap.set("n", "<Space>l", "$", opts)
+keymap.set("n", "st", "<Cmd>TodoTelescope<CR>", opts)
+-- Copy current buffer path(relative)
+keymap.set("n", "<C-c>", "<Cmd>let @+ = expand('%:.')<CR>", opts)
 
 -- Insert --
-keymap("i", "jk", "<ESC>", opts)
-keymap("i", ",", ",<Space>", opts) -- insert Space automatically after ","
+keymap.set("i", "jk", "<ESC>", opts)
+keymap.set("i", ",", ",<Space>", opts) -- insert Space automatically after ","
 
 -- Visual --
-keymap("v", "<C-p>", '"0p', opts)
-keymap("v", "<Space>h", "^", opts)
-keymap("v", "<space>l", "$", opts)
+keymap.set("v", "<C-p>", '"0p', opts)
+keymap.set("v", "<Space>h", "^", opts)
+keymap.set("v", "<Space>l", "$", opts)
 
 -- Command --
-keymap("c", "qq", "qa", opts) -- delete all windows
+keymap.set("c", "qq", "qa", opts) -- delete all windows
 -- completation
-keymap("c", "<C-j>", "<C-z>", opts)
-keymap("c", "<C-k>", "<S-tab>", opts)
-keymap("c", "<Tab>", "<Nop>", opts)
+keymap.set("c", "<C-j>", "<C-z>", opts)
+keymap.set("c", "<C-k>", "<S-tab>", opts)
+keymap.set("c", "<Tab>", "<Nop>", opts)
