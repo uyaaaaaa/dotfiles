f8f69636e9b79535180438a3e97c8061e88cb0df
uyaaaaaa <tosuyasocial48@gmail.com>
2024-09-15 13:13:58 +0900

    update: keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -10,24 +10,27 @@ vim.g.maplocalleader = " "
 -- Normal --
 -- window navigation
 keymap.set("n", "<C-h>", "<C-w>h", opts)
-keymap.set("n", "<C-j>", "<C-w>j", opts)
-keymap.set("n", "<C-k>", "<C-w>k", opts)
 keymap.set("n", "<C-l>", "<C-w>l", opts)
--- yank
-keymap.set("n", "Y", "y$", opts)
 -- Split window
-keymap.set("n", "sv", "<Cmd>split<CR><C-w>w", opts)
 keymap.set("n", "ss", "<Cmd>vsplit<CR><C-w>w", opts)
-keymap.set("n", "x", '"_x', opts) -- Do not yank with x
+-- Yank to last
+keymap.set("n", "Y", "y$", opts)
+-- Do not yank with x
+keymap.set("n", "x", '"_x', opts)
+-- Change to UPPER_CASE
+keymap.set("n", "<C-g><C-u>", "bgUl", { desc = "Change First Character To Upper Case" })
+keymap.set("n", "<C-g><C-k>", "gUiw", { desc = "Change Word To Upper Case" })
+-- Move line
 keymap.set("n", "<Space>h", "^", opts)
 keymap.set("n", "<Space>l", "$", opts)
+-- Search TODO
 keymap.set("n", "st", "<Cmd>TodoTelescope<CR>", opts)
 -- Copy current buffer path(relative)
 keymap.set("n", "<C-c>", "<Cmd>let @+ = expand('%:.')<CR>", opts)
--- move current line
+-- Move current line
 keymap.set("n", "<C-j>", "<Cmd>move+{v:count1}<CR>=l", opts)
 keymap.set("n", "<C-k>", "<Cmd>move-1-{v:count1}<CR>=l", opts)
--- copy current line
+-- Copy current line
 keymap.set("n", "<Space>j", "<Cmd>copy.<CR>", opts)
 keymap.set("n", "<Space>k", "<Cmd>copy-1<CR>", opts)
 
@@ -42,16 +45,17 @@ keymap.set("v", "<Space>h", "^", opts)
 keymap.set("v", "<Space>l", "$", opts)
 keymap.set("v", "p", "P", opts)
 keymap.set("v", "hl", "<ESC>", opts)
--- move selected line
+-- Move selected line
 keymap.set("v", "<C-j>", ":move'>+1<CR>gv=gv", opts)
 keymap.set("v", "<C-k>", ":move'<-2<CR>gv=gv", opts)
--- copy selected line
+-- Copy selected line
 keymap.set("v", "<Space>j", ":copy'<-1<CR>gv", opts)
 keymap.set("v", "<Space>k", ":copy'>+0<CR>gv", opts)
 
 -- Command --
 keymap.set("c", "qq", "qa", opts) -- delete all windows
--- completation
+-- Completation
 keymap.set("c", "<C-j>", "<C-z>", opts)
 keymap.set("c", "<C-k>", "<S-tab>", opts)
 keymap.set("c", "<Tab>", "<Nop>", opts)
+keymap.set("c", "jk", "<ESC>", opts)
