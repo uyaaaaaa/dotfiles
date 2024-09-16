c16b6aa5c9c1a3f0785833628141c6ddf6a487d3
uyaaaaaa <tosuyasocial48@gmail.com>
2024-09-16 11:21:35 +0900

    mod: keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -20,10 +20,10 @@ keymap.set("n", "x", '"_x', opts)
 -- Change to UPPER_CASE
 keymap.set("n", "<C-g><C-u>", "bgUl", { desc = "Change First Character To Upper Case" })
 keymap.set("n", "<C-g><C-k>", "gUiw", { desc = "Change Word To Upper Case" })
--- Move line
+-- Move inside line
 keymap.set("n", "<Space>h", "^", opts)
 keymap.set("n", "<Space>l", "$", opts)
--- Search TODO
+-- Search Todo
 keymap.set("n", "st", "<Cmd>TodoTelescope<CR>", opts)
 -- Copy current buffer path(relative)
 keymap.set("n", "<C-c>", "<Cmd>let @+ = expand('%:.')<CR>", opts)
@@ -33,20 +33,29 @@ keymap.set("n", "<C-k>", "<Cmd>move-1-{v:count1}<CR>=l", opts)
 -- Copy current line
 keymap.set("n", "<Space>j", "<Cmd>copy.<CR>", opts)
 keymap.set("n", "<Space>k", "<Cmd>copy-1<CR>", opts)
+-- Move inside buffer
+keymap.set("n", "gh", "H", opts)
+keymap.set("n", "gm", "M", opts)
+keymap.set("n", "gl", "L", opts)
 
 -- Insert --
 keymap.set("i", "jk", "<ESC>", opts)
 keymap.set("i", ",", ",<Space>", opts) -- insert Space automatically after ","
+-- Delete word
 keymap.set("i", "<C-d>", "<C-w>", opts)
+-- Delete character
 keymap.set("i", "<C-h>", "<BS>", opts)
 keymap.set("i", "<C-l>", "<Del>", opts)
+-- Insert new line
+keymap.set("i", "<C-j>", "<End><CR>", opts)
+keymap.set("i", "<C-k><C-o>", "<Home><CR><Up>", opts)
 
 -- Visual --
+keymap.set("v", "hl", "<ESC>", opts)
 keymap.set("v", "<C-p>", '"0p', opts)
 keymap.set("v", "<Space>h", "^", opts)
 keymap.set("v", "<Space>l", "$", opts)
 keymap.set("v", "p", "P", opts)
-keymap.set("v", "hl", "<ESC>", opts)
 -- Move selected line
 keymap.set("v", "<C-j>", ":move'>+1<CR>gv=gv", opts)
 keymap.set("v", "<C-k>", ":move'<-2<CR>gv=gv", opts)
@@ -60,4 +69,3 @@ keymap.set("c", "qq", "qa", opts) -- delete all windows
 keymap.set("c", "<C-j>", "<C-z>", opts)
 keymap.set("c", "<C-k>", "<S-tab>", opts)
 keymap.set("c", "<Tab>", "<Nop>", opts)
-keymap.set("c", "jk", "<ESC>", opts)
