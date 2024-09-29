e72852d86125bde1f64b84f2674d0cac63cf8636
uyaaaaaa <tosuyasocial48@gmail.com>
2024-09-29 14:10:57 +0900

    config: wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -18,6 +18,7 @@ config.font = wezterm.font("JetBrains Mono", { weight = "Medium", stretch = "Nor
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font_size = 11
 config.adjust_window_size_when_changing_font_size = false
+config.use_ime = true
 config.window_padding = {
 	left = 10,
 	right = 10,
@@ -38,6 +39,52 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 else
 	config.native_macos_fullscreen_mode = false
 end
+
+-- tab setting
+config.window_decorations = "RESIZE"
+config.window_frame = {
+	inactive_titlebar_bg = "none",
+	active_titlebar_bg = "none",
+}
+config.window_background_gradient = {
+	colors = { "#000000" },
+}
+config.show_new_tab_button_in_tab_bar = false
+config.show_close_tab_button_in_tabs = false
+config.colors = {
+	tab_bar = {
+		inactive_tab_edge = "none",
+	},
+}
+
+local SOLID_LEFT_ARROW = wezterm.nerdfonts.ple_lower_right_triangle
+local SOLID_RIGHT_ARROW = wezterm.nerdfonts.ple_upper_left_triangle
+
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+	local background = "#5c6d74"
+	local foreground = "#FFFFFF"
+	local edge_background = "none"
+
+	if tab.is_active then
+		background = "#483d8b"
+		foreground = "#FFFFFF"
+	end
+	local edge_foreground = background
+
+	local title = "   " .. wezterm.truncate_right(tab.active_pane.title, max_width - 1) .. "   "
+
+	return {
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_LEFT_ARROW },
+		{ Background = { Color = background } },
+		{ Foreground = { Color = foreground } },
+		{ Text = title },
+		{ Background = { Color = edge_background } },
+		{ Foreground = { Color = edge_foreground } },
+		{ Text = SOLID_RIGHT_ARROW },
+	}
+end)
 
 config.disable_default_key_bindings = true
 
