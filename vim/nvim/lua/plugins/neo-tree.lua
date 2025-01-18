5f4f216ed945e1092dcbc0cb77713d6fe24dd2a6
uyaaaaaa <tosuyasocial48@gmail.com>
2025-01-18 11:53:42 +0900

    update: NeoTree

--- vim/nvim/lua/plugins/neo-tree.lua
+++ vim/nvim/lua/plugins/neo-tree.lua
@@ -1,14 +1,41 @@
 return {
     'nvim-neo-tree/neo-tree.nvim',
     cmd = 'Neotree',
+    lazy = false,
     dependencies = {
         'nvim-lua/plenary.nvim',
         'nvim-tree/nvim-web-devicons',
         'MunifTanjim/nui.nvim',
     },
     keys = {
-      { '<leader>e', ':Neotree toggle<CR>' },
+        {
+            '<leader>e',
+            function()
+                require("neo-tree.command").execute({ toggle = true })
+            end,
+            desc = 'Explorer NeoTree',
+        },
     },
+    deactivate = function()
+      vim.cmd([[Neotree close]])
+    end,
+    init = function()
+        vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
+        desc = "Start Neo-tree with directory",
+        once = true,
+        callback = function()
+            if package.loaded["neo-tree"] then
+                return
+            else
+                local stats = vim.uv.fs_stat(vim.fn.argv(0))
+                if stats and stats.type == "directory" then
+                    require("neo-tree")
+                end
+            end
+        end,
+        })
+    end,
     opts = {
         sources = { 'filesystem', 'buffers', 'git_status' },
         open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
@@ -19,10 +46,13 @@ return {
         },
         window = {
             mappings = {
-                ['<space>'] = 'none',
+                ['<Space>'] = 'none',
+                ['q'] = 'none',
                 ['l'] = 'open',
                 ['h'] = 'close_node',
-                ["P"] = { "toggle_preview", config = { use_float = true, use_image_nvim = false } },
+                ['r'] = 'rename',
+                ['b'] = 'rename_basename',
+                ["P"] = { "toggle_preview", config = { use_float = false, use_image_nvim = false } },
                 ["<C-j>"] = { "scroll_preview", config = { direction = -10 } },
                 ["<C-k>"] = { "scroll_preview", config = { direction = 10 } },
             },
@@ -39,10 +69,8 @@ return {
                 },
             },
             filtered_items = {
-                always_show = {
-                    ".gitignored",
-                },
                 always_show_by_pattern = {
+                    ".gitignored",
                     ".env*",
                 },
                 never_show = {
@@ -51,5 +79,19 @@ return {
                 },
             },
         },
+        default_component_configs = {
+            indent = {
+                with_expanders = true,
+                expander_collapsed = "",
+                expander_expanded = "",
+                expander_highlight = "NeoTreeExpander",
+            },
+            git_status = {
+                symbols = {
+                    unstaged = "󰄱",
+                    staged = "󰱒",
+                },
+            },
+      },
     },
 }
