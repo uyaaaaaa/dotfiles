d204ff413991818bcc357720a09c5099e7ed0e3d
uyaaaaaa <tosuyasocial48@gmail.com>
2025-01-19 18:12:16 +0900

    move: autcmd of neotree

--- vim/nvim/lua/core/autocmds.lua
+++ vim/nvim/lua/core/autocmds.lua
@@ -1,5 +1,6 @@
 -- Create autocommand
 local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
 
 -- Remove whitespace on save
 autocmd("BufWritePre", {
@@ -19,6 +20,21 @@ autocmd("BufReadPost", {
         local lcount = vim.api.nvim_buf_line_count(buf)
         if mark[1] > 0 and mark[1] <= lcount then
             pcall(vim.api.nvim_win_set_cursor, 0, mark)
+        end
+    end,
+})
+autocmd("BufEnter", {
+    group = augroup("Neotree_start_directory", { clear = true } ),
+    desc = "Start NeoTree with directory",
+    once = true,
+    callback = function()
+        if package.loaded["neo-tree"] then
+            return
+        else
+            local stats = vim.uv.fs_stat(vim.fn.argv(0))
+            if stats and stats.type == "directory" then
+                require("neo-tree")
+            end
         end
     end,
 })
--- vim/nvim/lua/plugins/neo-tree.lua
+++ vim/nvim/lua/plugins/neo-tree.lua
@@ -19,23 +19,6 @@ return {
     deactivate = function()
       vim.cmd([[Neotree close]])
     end,
-    init = function()
-        vim.api.nvim_create_autocmd("BufEnter", {
-            group = vim.api.nvim_create_augroup("Neotree_start_directory", { clear = true }),
-            desc = "Start Neo-tree with directory",
-            once = true,
-            callback = function()
-                if package.loaded["neo-tree"] then
-                    return
-                else
-                    local stats = vim.uv.fs_stat(vim.fn.argv(0))
-                    if stats and stats.type == "directory" then
-                        require("neo-tree")
-                    end
-                end
-            end,
-        })
-    end,
     opts = {
         sources = { 'filesystem', 'buffers', 'git_status' },
         open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
