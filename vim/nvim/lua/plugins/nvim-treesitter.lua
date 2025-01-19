02d8c4d67c5ce31208f578ce06e8d8ee9ff058f7
uyaaaaaa <tosuyasocial48@gmail.com>
2025-01-19 12:00:41 +0900

    update: treesitter options

--- vim/nvim/lua/plugins/nvim-treesitter.lua
+++ vim/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,23 +5,37 @@ return {
     build = ":TSUpdate",
     event = { "VeryLazy" },
     lazy = vim.fn.argc(-1) == 0,
-    config = function ()
-        require("nvim-treesitter.configs").setup({
-            ensure_installed = {  -- if needed, add language.
-                "python",
-                "lua",
-                "vim",
-                "query",
-                "sql",
-                "php",
-                "javascript",
-                "typescript",
-                "html",
-                "css",
-                "diff",
-            },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
+    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    opts_extend = { "ensure_installed" },
+    ---@type TSConfig
+    ---@diagnostic disable-next-line: missing-fields
+    opts = {
+        highlight = { enable = true },
+        indent = { enable = true },
+        ensure_installed = {  -- if needed, add language.
+            "bash",
+            "jsdoc",
+            "json",
+            "markdown",
+            "printf",
+            "python",
+            "lua",
+            "vim",
+            "query",
+            "sql",
+            "php",
+            "tsx",
+            "javascript",
+            "typescript",
+            "html",
+            "css",
+            "diff",
+            "xml",
+            "yaml",
+        },
+    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+        require("nvim-treesitter.configs").setup(opts)
     end,
 }
