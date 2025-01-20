dbcb3c6c03697a240fbafc30f45dcfe6d2bb0104
uyaaaaaa <tosuyasocial48@gmail.com>
2025-01-20 21:36:42 +0900

    chore

--- vim/nvim/lua/plugins/neo-tree.lua
+++ vim/nvim/lua/plugins/neo-tree.lua
@@ -22,11 +22,6 @@ return {
     opts = {
         sources = { 'filesystem', 'buffers', 'git_status' },
         open_files_do_not_replace_types = { 'terminal', 'Trouble', 'trouble', 'qf', 'Outline' },
-        filesystem = {
-            bind_to_cwd = false,
-            follow_current_file = { enabled = true },
-            use_libuv_file_watcher = true,
-        },
         window = {
             mappings = {
                 ['<Space>'] = 'none',
@@ -42,6 +37,9 @@ return {
             close_if_last_window = true,
         },
         filesystem = {
+            bind_to_cwd = false,
+            follow_current_file = { enabled = true },
+            use_libuv_file_watcher = true,
             window = {
                 mappings = {
                      ["<C-f>"] = "clear_filter",
