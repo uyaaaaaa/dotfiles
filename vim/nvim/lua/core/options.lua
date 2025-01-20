5a0349ae5eb1cc5a0a5fbef2498caf18a62e92e0
uyaaaaaa <tosuyasocial48@gmail.com>
2025-01-20 22:01:34 +0900

    chore: set options

--- vim/nvim/lua/core/options.lua
+++ vim/nvim/lua/core/options.lua
@@ -4,7 +4,7 @@ opt.encoding = 'utf-8'
 opt.fileencoding = 'utf-8'
 opt.number = true
 opt.relativenumber = false
-
+opt.confirm = true
 -- Tab & Indent
 opt.tabstop = 4
 opt.shiftwidth = 4
@@ -12,6 +12,15 @@ opt.shiftround = true
 opt.expandtab = true
 opt.smartindent = true
 opt.autoindent = true  -- apply indent for new line
+opt.laststatus = 3
+opt.linebreak = true
+opt.sessionoptions = { "buffers", "curdir", "tabpages", "winsize", "help", "globals", "skiprtp", "folds" }
+opt.sidescrolloff = 8
+opt.splitright = true
+opt.timeoutlen = vim.g.vscode and 1000 or 300
+opt.undofile = true
+opt.undolevels = 10000
+opt.wrap = false
 
 -- Fold
 function Foldtext()
