66151918165022be109443396cccd5ba05cc55a8
uyaaaaaa <tosuyasocial48@gmail.com>
2025-01-27 08:36:26 +0900

    add: nvim-autopairs

--- vim/nvim/lua/plugins/coding.lua
+++ vim/nvim/lua/plugins/coding.lua
@@ -40,4 +40,16 @@ return {
             require("nvim-treesitter.configs").setup(opts)
         end,
     },
+
+    -- auto pairs
+    {
+        "windwp/nvim-autopairs",
+        event = "InsertEnter",
+        config = true,
+        opts = {
+            disable_filetype = { "TelescopePrompt", "spectre_panel" },
+            map_c_h = true, -- Map the <C-h> key to delete a pair
+            map_c_w = true, -- Map <c-w> to delete a pair if possible
+        },
+    }
 }
