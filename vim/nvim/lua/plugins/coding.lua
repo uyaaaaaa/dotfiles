885601ae8b0d8bb580fdd92a39f052ad658bf505
uyaaaaaa <tosuyasocial48@gmail.com>
2025-01-30 08:44:51 +0900

    add: flash.nvim

--- vim/nvim/lua/plugins/coding.lua
+++ vim/nvim/lua/plugins/coding.lua
@@ -9,5 +9,21 @@ return {
             map_c_h = true, -- Map the <C-h> key to delete a pair
             map_c_w = true, -- Map <c-w> to delete a pair if possible
         },
+    },
+
+    -- flash.nvim
+    {
+        "folke/flash.nvim",
+        event = "VeryLazy",
+        vscode = true,
+        ---@type Flash.Config
+        opts = {},
+        keys = {
+            { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            -- Flash With Motion (move and operation)
+            { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+            { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+        },
     }
 }
