32b51298c5fe751e18e4215bccb09db0c9f35319
uyaaaaaa <tosuyasocial48@gmail.com>
2025-02-11 15:10:37 +0900

    add: noice

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -409,4 +409,50 @@ return {
         end,
     },
 
+    -- noice
+    {
+        "folke/noice.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            "rcarriga/nvim-notify",
+        },
+        opts = {
+            lsp = {
+                override = {
+                    ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                    ["vim.lsp.util.stylize_markdown"] = true,
+                    ["cmp.entry.get_documentation"] = true,
+                },
+            },
+            routes = {
+                {
+                    filter = {
+                        event = "msg_show",
+                        kind = "",
+                        any = {
+                            { find = "%d+L, %d+B" },
+                            { find = "; after #%d+" },
+                            { find = "; before #%d+" },
+                        },
+                    },
+                    opts = { skip = true },
+                },
+                {
+                    filter = {
+                        event = "notify",
+                        find = "No information available",
+                    },
+                    opts = { skip = true },
+                },
+            },
+            presets = {
+                command_palette = true,
+                lsp_doc_border = true,
+            },
+        },
+        config = function(_, opts)
+            require("noice").setup(opts)
+        end,
+    },
 }
