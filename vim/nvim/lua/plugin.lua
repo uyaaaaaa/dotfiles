e1d23fda369216042d14772d8e0065d66ad3b598
uyaaaaaa <tosuyasocial48@gmail.com>
2025-02-18 08:34:19 +0900

    move: nvim-cmp

--- vim/nvim/lua/plugin.lua
+++ vim/nvim/lua/plugin.lua
@@ -21,7 +21,7 @@ require("lazy").setup({
         { import = "plugins.editor" },
         { import = "plugins.coding" },
         { import = "plugins.markdown" },
-        { import = "plugins.lspconfig" },
+        { import = "plugins.completion" },
         { import = "plugins.picker" },
     },
     diff = {
---
+++ vim/nvim/lua/plugins/completion.lua
@@ -0,0 +1,138 @@
+return {
+    -- nvim-cmp
+    {
+        "hrsh7th/nvim-cmp",
+        version = false,
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "onsails/lspkind.nvim",
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                experimental = { ghost_text = true },
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                formatting = {
+                    format = require("lspkind").cmp_format({
+                        mode = "symbol",
+                        maxwidth = 50,
+                        ellipsis_char = "...",
+                        before = function(entry, vim_item)
+                            return vim_item
+                        end,
+                    }),
+                },
+                window = {
+                    completion = {
+                        border = "rounded",
+                        zindex = 1000,
+                        side_padding = 1,
+                        winblend = 20,
+                    },
+                    documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "render-markdown" },
+                }, {
+                    { name = "buffer" },
+                    { name = "path" },
+                    { name = "cmd"},
+                }),
+            })
+            -- Set up lspconfig.
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            -- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+            require("lspconfig")["php-cs-fixer"].setup({
+                capabilities = capabilities,
+            })
+        end,
+    },
+
+    -- lspconfig
+    {
+        "neovim/nvim-lspconfig",
+        event = "BufRead",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+        keys = {
+            -- { "gd", function() require("telescope.builtin").lsp_definitions() end, mode = "n", desc = "Go to definition" }
+            { "gd", function() Snacks.picker.lsp_definitions() end, mode = "n", desc = "Go to definition" }
+        },
+        config = function()
+            -- local lspconfig = require('lspconfig')
+            -- lspconfig.pyright.setup{}
+            -- lspconfig.phpactor.setup{}
+            -- lspconfig.intelephense.setup{}
+        end,
+    },
+
+    -- mason
+    {
+        "williamboman/mason.nvim",
+        cmd = "Mason",
+        keys = {
+            { "<leader>mm", "<cmd>Mason<cr>", desc = "Mason" }
+        },
+        build = ":MasonUpdate",
+        opts_extend = { "ensure_installed" },
+        opts = {
+            ensure_installed = {
+                "stylua",
+                "shfmt",
+            },
+        },
+        ---@param opts MasonSettings | {ensure_installed: string[]}
+        config = function(_, opts)
+            require("mason").setup(opts)
+            local mr = require("mason-registry")
+            mr:on("package:install:success", function()
+                vim.defer_fn(function()
+                    -- trigger FileType event to possibly load this newly installed LSP server
+                    require("lazy.core.handler.event").trigger({
+                        event = "FileType",
+                        buf = vim.api.nvim_get_current_buf(),
+                    })
+                end, 100)
+            end)
+
+            mr.refresh(function()
+                for _, tool in ipairs(opts.ensure_installed) do
+                    local p = mr.get_package(tool)
+                    if not p:is_installed() then
+                        p:install()
+                    end
+                end
+            end)
+        end,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        event = "LspAttach",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason").setup()
+            require("mason-lspconfig").setup()
+        end
+    },
