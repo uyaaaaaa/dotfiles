938b6165194967d84cc44d6b7eaeda1590e90566
uyaaaaaa <tosuyasocial48@gmail.com>
2025-02-21 08:46:38 +0900

    update: trouble

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -400,9 +400,47 @@ return {
             require("noice").setup(opts)
         end,
     },
+
     -- trouble
     {
         "folke/trouble.nvim",
         cmd = { "Trouble" },
+        opts = {
+            modes = {
+                lsp = {
+                   win = { position = "right" },
+                },
+            },
+        },
+        keys = {
+            {
+                "[q",
+                function()
+                    if require("trouble").is_open() then
+                        require("trouble").prev({ skip_groups = true, jump = true })
+                    else
+                        local ok, err = pcall(vim.cmd.cprev)
+                        if not ok then
+                            vim.notify(err, vim.log.levels.ERROR)
+                        end
+                    end
+                end,
+                desc = "Previous Trouble/Quickfix Item",
+            },
+            {
+                "]q",
+                function()
+                    if require("trouble").is_open() then
+                        require("trouble").next({ skip_groups = true, jump = true })
+                    else
+                        local ok, err = pcall(vim.cmd.cnext)
+                        if not ok then
+                            vim.notify(err, vim.log.levels.ERROR)
+                        end
+                    end
+                end,
+                desc = "Next Trouble/Quickfix Item",
+            },
+        },
     },
 }
