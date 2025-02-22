679e23755cd5d6cd33990b72e72c36e09c5cd9aa
uyaaaaaa <tosuyasocial48@gmail.com>
2025-02-22 18:38:58 +0900

    chore: disable diagnostics

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -151,6 +151,7 @@ return {
             })
         end,
         opts = {
+            enable_diagnostics = false,
             sources = { "filesystem", "buffers", "git_status" },
             open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf", "Outline" },
             hide_root_node = true,
