d810aaa27eec5a6e75584b8db5cf8d18086bfd14
uyaaaaaa <tosuyasocial48@gmail.com>
2025-02-23 20:47:30 +0900

    add: null-ls

--- vim/nvim/lua/plugins/lspconfig.lua
+++ vim/nvim/lua/plugins/lspconfig.lua
@@ -35,6 +35,48 @@ return {
         end,
     },
 
+    -- mason-null-ls
+    {
+        "jay-babu/mason-null-ls.nvim",
+        cmd = "Mason",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+        config = function ()
+            require("mason-null-ls").setup({
+                automatic_setup = true,
+                ensure_installed = vim.tbl_flatten({ lsp.formatters, lsp.diagnostics }),
+            })
+        end,
+    },
+
+    -- none-ls
+    {
+        "nvimtools/none-ls.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        config = function()
+            local null_ls = require("null-ls")
+
+            local formatting_sources = {}
+            for _, tool in ipairs(lsp.formatters) do
+                table.insert(formatting_sources, null_ls.builtins.formatting[tool])
+            end
+
+            local diagnostics_sources = {}
+            for _, tool in ipairs(lsp.diagnostics) do
+                table.insert(diagnostics_sources, null_ls.builtins.diagnostics[tool])
+            end
+
+            -- none-ls setup
+            null_ls.setup({
+                diagnostics_format = "[#{m}] #{s} (#{c})",
+                sources = vim.tbl_flatten({ formatting_sources, diagnostics_sources }),
+            })
+        end,
+    },
+
     -- -- mason-nvim-dap
     -- {
     --     "jay-babu/mason-nvim-dap.nvim",
