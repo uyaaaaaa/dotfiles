496ba9e99a5b45ab40fd24aa488283c55c5f3805
uyaaaaaa <tosuyasocial48@gmail.com>
2025-02-23 20:53:27 +0900

    chore: update icons

--- vim/nvim/lua/utils/icons.lua
+++ vim/nvim/lua/utils/icons.lua
@@ -1,22 +1,25 @@
-return {
-    error_icon = " ",
-    warn_icon = " ",
-    hint_icon = "󰌵 ",
-    info_icon = " ",
-    modified_icon = " ",
-    added_icon = " ",
-    removed_icon = " ",
-    deleted_icon = "",
-    renamed_icon = "󰛂 ",
-    untracked_icon = " ",
-    ignored_icon = " ",
-    unstaged_icon = " ",
-    staged_icon = " ",
-    conflict_icon = "",
-    code_icon = " ",
-    win_icon = " ",
-    linux_icon = " ",
-    mac_icon = " ",
-    readonly_icon = "󰌾 ",
-    branch_icon = " ",
-}
+---@class lazyvim.util.icons
+local M = {}
+
+M.error_icon = " "
+M.warn_icon = " "
+M.hint_icon = "󰌵 "
+M.info_icon = " "
+M.modified_icon = " "
+M.added_icon = " "
+M.removed_icon = " "
+M.deleted_icon = ""
+M.renamed_icon = "󰛂 "
+M.untracked_icon = " "
+M.ignored_icon = " "
+M.unstaged_icon = " "
+M.staged_icon = " "
+M.conflict_icon = ""
+M.code_icon = " "
+M.win_icon = " "
+M.linux_icon = " "
+M.mac_icon = " "
+M.readonly_icon = "󰌾 "
+M.branch_icon = " "
+
+return M
