3f668a53dea7576c49ed202b5e9a920495f3f9a5
uyaaaaaa <tosuyasocial48@gmail.com>
2025-02-25 22:13:17 +0900

    add: intelephense

---
+++ vim/nvim/lua/plugins/lang/php/intelephense.lua
@@ -0,0 +1,28 @@
+return {
+    "neovim/nvim-lspconfig",
+    ft = { "blade", "php" },
+    config = function()
+        local lspconfig = require("lspconfig")
+        lspconfig.intelephense.setup({
+            capabilities = capabilities,
+            on_attach = on_attach,
+            root_dir = function(fname)
+                return lsp_config.util.find_git_ancestor(fname) or vim.fn.getcwd()
+            end,
+            settings = {
+                intelephense = {
+                    files = {
+                        associations = {
+                            "*.php",
+                        },
+                        includePaths = {
+                            "./_ide_helper.php",
+                            "./_ide_helper_models.php",
+                        },
+                    },
+                },
+            },
+        })
+    end,
+}
+
