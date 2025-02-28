e12c18ff3b5a00340439cf954bd95384fb7fbf1e
uyaaaaaa <tosuyasocial48@gmail.com>
2025-02-28 21:43:25 +0900

    update: lualine

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -494,8 +494,37 @@ return {
         "akinsho/bufferline.nvim",
         version = "*",
         event = "VeryLazy",
+        keys = {
+            { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+            { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+        },
         dependencies = {
             "nvim-tree/nvim-web-devicons",
+        },
+        opts = {
+            options = {
+                close_command = function(n) Snacks.bufdelete(n) end,
+                right_mouse_command = function(n) Snacks.bufdelete(n) end,
+                diagnostics = "nvim_lsp",
+                always_show_bufferline = false,
+                -- diagnostics_indicator = function(_, _, diag)
+                --     local icons = vim.config.icons.diagnostics
+                --     local ret = (diag.error and icons.Error .. diag.error .. " " or "") .. (diag.warning and icons.Warn .. diag.warning or "")
+                --     return vim.trim(ret)
+                -- end,
+                offsets = {
+                    {
+                        filetype = "neo-tree",
+                        text = "Neo-tree",
+                        highlight = "Directory",
+                        text_align = "left",
+                    },
+                },
+                ---@param opts bufferline.IconFetcherOpts
+                -- get_element_icon = function(opts)
+                --     return LazyVim.config.icons.ft[opts.filetype]
+                -- end,
+            },
         },
         config = function(_, opts)
             require("bufferline").setup(opts)
