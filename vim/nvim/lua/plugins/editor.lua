70f25f7c0272cf2fd917c68a2d4908c417e9df60
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-01 16:47:41 +0900

    remove: options

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -507,11 +507,6 @@ return {
                 right_mouse_command = function(n) Snacks.bufdelete(n) end,
                 diagnostics = "nvim_lsp",
                 always_show_bufferline = false,
-                -- diagnostics_indicator = function(_, _, diag)
-                --     local icons = vim.config.icons.diagnostics
-                --     local ret = (diag.error and icons.Error .. diag.error .. " " or "") .. (diag.warning and icons.Warn .. diag.warning or "")
-                --     return vim.trim(ret)
-                -- end,
                 offsets = {
                     {
                         filetype = "neo-tree",
@@ -520,10 +515,6 @@ return {
                         text_align = "left",
                     },
                 },
-                ---@param opts bufferline.IconFetcherOpts
-                -- get_element_icon = function(opts)
-                --     return LazyVim.config.icons.ft[opts.filetype]
-                -- end,
             },
         },
         config = function(_, opts)
