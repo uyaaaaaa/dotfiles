df32366259b1499bed18a2570a949a9cfbaef4de
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-01 15:13:11 +0900

    add: abbreviations config

---
+++ vim/nvim/lua/abbreviations.lua
@@ -0,0 +1,11 @@
+-- Setup abbreviations
+vim.cmd([[
+    "Insert mode
+    iabbrev ymd <C-R>=strftime("%Y%m%d")<CR>
+    iabbrev ymd- <C-R>=strftime("%Y-%m-%d")<CR>
+
+    "Command mode
+    cabbrev <expr> w] (getcmdtype() ==# ":" && getcmdline() ==# "w]") ? "w" : "w]"
+    cabbrev <expr> w (getcmdtype() ==# ":" && getcmdline() ==# "'<,'>w") ? "\<C-u>w" : "w"
+    cnoreabbrev <expr> s getcmdtype() .. getcmdline() ==# ':s' ? [getchar(), ''][1] .. "%s///g<Left><Left>" : 's'
+]])
