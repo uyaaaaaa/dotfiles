45772d68c782e10bf87a444eac521e3c03900343
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-03 07:39:35 +0900

    mod: cmp symbol

--- vim/nvim/lua/plugins/completion.lua
+++ vim/nvim/lua/plugins/completion.lua
@@ -25,7 +25,7 @@ return {
                 },
                 formatting = {
                     format = require("lspkind").cmp_format({
-                        mode = "symbol",
+                        mode = "symbol_text",
                         maxwidth = 50,
                         ellipsis_char = "...",
                         before = function(entry, vim_item)
