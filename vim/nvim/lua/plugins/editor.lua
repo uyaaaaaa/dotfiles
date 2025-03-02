3e81d22c1d9a6f328a5fd85d02a10ddec523dbfd
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-02 11:19:25 +0900

    add: lualine

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -525,4 +525,21 @@ return {
             },
         },
     },
+
+    -- lualine
+    {
+        "nvim-lualine/lualine.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        event = "VeryLazy",
+        init = function()
+            vim.g.lualine_laststatus = vim.o.laststatus
+            if vim.fn.argc(-1) > 0 then
+                vim.o.statusline = " "
+            else
+                vim.o.laststatus = 0
+            end
+        end,
+    },
 }
