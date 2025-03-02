ab248020a2dfa4db0007d06c94c3afd07679d62e
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-02 13:46:10 +0900

    update: lualine

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -541,5 +541,79 @@ return {
                 vim.o.laststatus = 0
             end
         end,
+        opts = function()
+            require("lualine_require").require = require
+            vim.o.laststatus = vim.g.lualine_laststatus
+            local icons = require("utils.icons")
+            local git_repo = vim.fn.fnamemodify(vim.fn.systemlist('git rev-parse --show-toplevel')[1], ':t') or ''
+
+            local opts = {
+                options = {
+                    component_separators = { left = "", right = "" },
+                    section_separators = { left = "", right = "" },
+                    globalstatus = vim.o.laststatus == 3,
+                    disabled_filetypes = { statusline = { "dashboard", "alpha", "ministarter", "snacks_dashboard" } },
+
+                },
+                sections = {
+                    lualine_a = { "mode" },
+                    lualine_b = {
+                        {
+                            function() return git_repo end,
+                            icon = icons.repository,
+                            color = function() return { fg = Snacks.util.color("Statement") } end,
+                            padding = { left = 1, right = 0 },
+                        },
+                        {
+                            "branch",
+                            icon = icons.branch,
+                            color = function() return { fg = Snacks.util.color("Constant") } end,
+                        },
+                    },
+                    lualine_c = {
+                        { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                        { "filename", path = 1, padding = { lefg = 0, right = 1 } },
+                    },
+                    lualine_x = {
+                        {
+                            function() return require("noice").api.status.command.get() end,
+                            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+                            color = function() return { fg = Snacks.util.color("Statement") } end,
+                            separator = "",
+                        },
+                        {
+                            function() return "  " .. require("dap").status() end,
+                            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+                            color = function() return { fg = Snacks.util.color("Debug") } end,
+                        },
+                        {
+                            "diagnostics",
+                            sections = { "error", "warn" },
+                            symbols = {
+                                error = icons.error,
+                                warn  = icons.warn,
+                                info  = icons.info,
+                                hint  = icons.hint,
+                            },
+                            always_visible = true,
+                            separator = "",
+                        },
+                        "encoding",
+                    },
+                    lualine_y = {
+                        { "progress", separator = " ", padding = { left = 1, right = 0 } },
+                        { "location", padding = { left = 0, right = 1 } },
+                    },
+                    lualine_z = {
+                        function()
+                            return " " .. os.date("%R")
+                        end,
+                    },
+                },
+                extensions = { "neo-tree", "lazy", "fzf", "toggleterm" },
+            }
+
+            return opts
+        end,
     },
 }
