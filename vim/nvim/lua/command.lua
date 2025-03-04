dc5602621e87e4385ad9e7d9119e924f4efa4964
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-05 07:17:31 +0900

    add: command of edit snippet

--- vim/nvim/lua/command.lua
+++ vim/nvim/lua/command.lua
@@ -1,4 +1,16 @@
 -- Setup Custom Commands
 local command = vim.api.nvim_create_user_command
 
+local function get_snippet_path(ft)
+    return vim.fn.expand("~/.config/nvim/lua/snippets/" .. ft .. ".lua")
+end
+
+local function edit_snippet()
+    local ft = vim.bo.filetype
+    local snippet_path = get_snippet_path(ft)
+
+    vim.cmd("edit " .. snippet_path)
+end
+
+command("SnipEdit", edit_snippet, { desc = "Edit snippet" })
 command("Fm", function() vim.lsp.buf.format() end, { desc = "Save with format" })
