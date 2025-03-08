313a1a3b56a4ab2d11d8870d9c536268b543a8df
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-08 22:15:03 +0900

    add: nightfox

--- vim/nvim/lua/plugins/colorscheme.lua
+++ vim/nvim/lua/plugins/colorscheme.lua
@@ -53,28 +53,28 @@ return {
     },
 
     -- nightfox
-    -- {
-    --     "EdenEast/nightfox.nvim",
-    --     lazy = true,
-    --     opts = {
-    --         options = {
-    --             transparent = true,
-    --             styles = {
-    --                 comments = "italic",
-    --                 conditionals = "italic",
-    --                 functions = "italic",
-    --                 keywords = "italic",
-    --             },
-    --         },
-    --     },
-    --     config = function(_, opts)
-    --         require("nightfox").setup(opts)
-    --         -- vim.cmd.colorscheme("duskfox")
-    --         -- vim.cmd.colorscheme("nightfox")
-    --         -- vim.cmd.colorscheme("nordfox")
-    --         vim.cmd.colorscheme("carbonfox")
-    --     end,
-    -- },
+    {
+        "EdenEast/nightfox.nvim",
+        lazy = true,
+        opts = {
+            options = {
+                transparent = true,
+                styles = {
+                    comments = "italic",
+                    conditionals = "italic",
+                    functions = "italic",
+                    keywords = "italic",
+                },
+            },
+        },
+        config = function(_, opts)
+            require("nightfox").setup(opts)
+            -- vim.cmd.colorscheme("duskfox")
+            -- vim.cmd.colorscheme("nightfox")
+            -- vim.cmd.colorscheme("nordfox")
+            vim.cmd.colorscheme("carbonfox")
+        end,
+    },
 
     -- onedark
     {
