83098a1662ce13afe012b2c03b8a465472b61c04
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-13 07:23:53 +0900

    add: noice

--- vim/nvim/lua/utils/noice.lua
+++ vim/nvim/lua/utils/noice.lua
@@ -1,49 +1,31 @@
 ---@class lazyvim.util.noice
 local M = {}
 
-local ignore_lists = {
-    {
-        event = "msg_show",
-        msg = {
-            "%d+L, %d+B",
-            "; after #%d+",
-            "; before #%d+",
-            "more lines",
-            "fewer lines",
-            -- "/",
-            "No information available",
-            "1 time",
-        },
-    },
-    {
-        event = "notify",
-        msg = {
-            "hover is not supported by the servers of the current buffer",
-            "No information available",
-        },
-    },
+local filters = {
+    { "msg_show", "%d+L, %d+B" },
+    { "msg_show", "; after #%d+" },
+    { "msg_show", "; before #%d+" },
+    { "msg_show", "more lines" },
+    { "msg_show", "fewer lines" },
+    { "msg_show", "No information available" },
+    { "msg_show", "1 time" },
+    { "notify", "hover is not supported by the servers of the current buffer" },
+    { "notify", "No information available" },
 }
+
+local function noiceWrapper(event, pattern)
+    return {
+        filter = {
+            event = event,
+            find = pattern,
+        },
+    }
+end
 
 M.routes = {}
 
--- format ignore lists
-for _, data in ipairs(ignore_lists) do
-    local messages = {}
-    for _, msg in ipairs(data.msg) do
-        local t = {}
-        t.find = msg
-        table.insert(messages, t)
-    end
-
-    local value = {
-        filter = {
-            event = data.event,
-            any = messages,
-        },
-        opts = { skip = true },
-    }
-
-    table.insert(M.routes, value)
+for _, values in pairs(filters) do
+    table.insert(M.routes, noiceWrapper(values[1], values[2]))
 end
 
 return M
