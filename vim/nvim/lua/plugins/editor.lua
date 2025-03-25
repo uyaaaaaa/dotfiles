ef5b03b882af84e791321d9b3a17b421fdf6394a
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-26 08:08:39 +0900

    chore: add description

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -78,7 +78,7 @@ return {
     {
         "trevorhauter/gitportal.nvim",
         keys = {
-            { "<leader>gp", mode = { "n", "v" }, function() return require("gitportal").open_file_in_browser() end, desc = "" },
+            { "<leader>gp", mode = { "n", "v" }, function() return require("gitportal").open_file_in_browser() end, desc = "Open in Github" },
         },
         opts = {
             switch_branch_or_commit_upon_ingestion = "ask_first",
