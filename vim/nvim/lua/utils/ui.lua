028fd94d9f1b8acaa3706069ef97e8dea6582ce4
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-29 19:03:17 +0900

    remove: unnecessary

--- vim/nvim/lua/utils/ui.lua
+++ vim/nvim/lua/utils/ui.lua
@@ -14,22 +14,6 @@ vim.diagnostic.config({
     },
 })
 
--- optimized treesitter foldexpr for Neovim >= 0.10.0
-function M.foldexpr()
-    local buf = vim.api.nvim_get_current_buf()
-    if vim.b[buf].ts_folds == nil then
-        if vim.bo[buf].filetype == "" then
-            return "0"
-        end
-        if vim.bo[buf].filetype:find("dashboard") then
-            vim.b[buf].ts_folds = false
-        else
-            vim.b[buf].ts_folds = pcall(vim.treesitter.get_parser, buf)
-        end
-    end
-    return vim.b[buf].ts_folds and vim.treesitter.foldexpr() or "0"
-end
-
 -- remap `gd`
 function M.GD()
     local line = vim.fn.getline(".")
@@ -52,17 +36,5 @@ function M.GD()
 
     Snacks.picker.lsp_definitions()
 end
-
--- -- set statuscolumn with fold
--- function M.get_statuscol()
---     local lnum = vim.v.lnum
---     local fold = " "
---
--- 	if vim.fn.foldlevel(lnum) > vim.fn.foldlevel(lnum - 1) then
---         fold = vim.fn.foldclosed(lnum) == -1 and fcs.foldopen or fcs.foldclose
---     end
---
--- 	return " %T%=%l %s" .. fold .. " "
--- end
 
 return M
