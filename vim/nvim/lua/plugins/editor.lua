bea921a571fb7a6ad7c847f1dca2ad37372354a2
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-30 10:18:05 +0900

    update: fold rules

--- vim/nvim/lua/plugins/editor.lua
+++ vim/nvim/lua/plugins/editor.lua
@@ -524,14 +524,17 @@ return {
         },
         event = "BufRead",
         opts = {
+            open_fold_hl_timeout = 200,
             provider_selector = function(bufnr, filetype, buftype)
-                return {'treesitter', 'indent'}
+                return { "treesitter", "indent" }
             end,
         },
         config = function (_, opts)
-            vim.keymap.set('n', 'zz', require('ufo').openAllFolds)
-            vim.keymap.set('n', 'zm', require('ufo').closeAllFolds)
-            require('ufo').setup(opts)
-        end
+            vim.keymap.set("n", "zm", require("ufo").closeAllFolds)
+            vim.keymap.set("n", "zz", require("ufo").openAllFolds)
+            vim.keymap.set("n", "zf", "za", { noremap = true, silent = true })    -- Toggle
+            vim.keymap.set("n", "zr", "zO", { noremap = true, silent = true })    -- Open Fold Under level
+            require("ufo").setup(opts)
+        end,
     },
 }
