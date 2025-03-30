df6d0fb77df44715fd24b35883e50a7a162115c4
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-31 07:57:35 +0900

    update: functoins

--- vim/nvim/lua/utils/ui.lua
+++ vim/nvim/lua/utils/ui.lua
@@ -16,25 +16,36 @@ vim.diagnostic.config({
 
 -- remap `gd`
 function M.GD()
-    local line = vim.fn.getline(".")
-    local col = vim.fn.col(".")
+    local cfile = vim.fn.expand("<cfile>")
 
-    for url in line:gmatch("%b()") do
-        url = url:sub(2, -2)
-        local start_pos = line:find(url, 1, true)
-        local end_pos = start_pos + #url
-
-        if col >= start_pos and col <= end_pos then
-            local cut_url = url:match("^(https?://[^%s]+)") or url
-
-            if cut_url:match("^https?://") then
-                vim.fn.jobstart({ "xdg-open", url }, { detach = true })
-                return
-            end
-        end
+    if cfile:match("^https?://") then
+        vim.ui.open(cfile)
+    else
+        Snacks.picker.lsp_definitions()
     end
+end
 
-    Snacks.picker.lsp_definitions()
+function M.OpenMarkdownHelp()
+    local buf = vim.api.nvim_create_buf(false, true)
+    vim.api.nvim_buf_set_option(buf, "filetype", "markdown")
+
+    local lines = vim.fn.readfile(vim.fn.expand("~/.config/nvim/doc/keymaps.md"))
+    vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
+
+    local width = math.floor(vim.o.columns * 0.8)
+    local height = math.floor(vim.o.lines * 0.8)
+    local row = math.floor((vim.o.lines - height) / 2)
+    local col = math.floor((vim.o.columns - width) / 2)
+
+    vim.api.nvim_open_win(buf, true, {
+        relative = "editor",
+        row = row,
+        col = col,
+        width = width,
+        height = height,
+        style = "minimal",
+        border = "rounded",
+    })
 end
 
 return M
