3efcda3fe52d6e63a65546288e86c593db41d14d
uyaaaaaa <tosuyasocial48@gmail.com>
2025-03-31 07:54:49 +0900

    update: add fold rules

--- vim/nvim/after/ftplugin/php.lua
+++ vim/nvim/after/ftplugin/php.lua
@@ -43,3 +43,6 @@ require("lspconfig").intelephense.setup({
         },
     },
 })
+
+vim.treesitter.query.set("php", "folds", "([(if_statement) (switch_statement) (while_statement) (do_statement) (for_statement) (foreach_statement) (try_statement) (function_definition) (class_declaration) (interface_declaration) (trait_declaration) (enum_declaration) (function_static_declaration) (method_declaration) (namespace_use_declaration)+ (property_element) (array_creation_expression)] @fold)")
+
