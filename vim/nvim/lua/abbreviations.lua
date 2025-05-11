8478fc4212a5696d839ad8b61934704bdb246f8d
uyaaaaaa <tosuyasocial48@gmail.com>
2025-05-12 07:54:39 +0900

    add: yyyymmdd_

--- vim/nvim/lua/abbreviations.lua
+++ vim/nvim/lua/abbreviations.lua
@@ -3,6 +3,7 @@ vim.cmd([[
     "Insert mode
     iabbrev ymd <C-R>=strftime("%Y%m%d")<CR>
     iabbrev ymd- <C-R>=strftime("%Y-%m-%d")<CR>
+    iabbrev ymd_ <C-R>=strftime("%Y%m%d_")<CR>
 
     "Command mode
     cabbrev <expr> w] (getcmdtype() ==# ":" && getcmdline() ==# "w]") ? "w" : "w]"
