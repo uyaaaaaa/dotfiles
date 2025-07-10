fb1fa7ebdf3264930bce23e7ab3b37f4f052ffc9
uyaaaaaa <tosuyasocial48@gmail.com>
2025-07-10 23:26:56 +0900

    mod: default colorscheme

--- vim/nvim/lua/plugins/colorscheme.lua
+++ vim/nvim/lua/plugins/colorscheme.lua
@@ -61,7 +61,7 @@ return {
         },
         config = function(_, opts)
             require("nightfox").setup(opts)
-            vim.cmd.colorscheme("nightfox")
+            vim.cmd.colorscheme("duskfox")
         end,
     },
 
