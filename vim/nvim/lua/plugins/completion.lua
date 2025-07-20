1c377d6d0e19b30d9c8ddd37851fd50571eb155f
uyaaaaaa <tosuyasocial48@gmail.com>
2025-07-20 11:43:10 +0900

    chore: modify keymap

--- vim/nvim/lua/plugins/completion.lua
+++ vim/nvim/lua/plugins/completion.lua
@@ -47,13 +47,13 @@ return {
                     documentation = cmp.config.window.bordered(),
                 },
                 mapping = cmp.mapping.preset.insert({
-                    ["<C-p>"] = cmp.mapping.select_prev_item(),
-                    ["<C-n>"] = cmp.mapping.select_next_item(),
-                    ["<C-f>"] = cmp.mapping.confirm({ select = true }),
+                    ["<C-k>"] = cmp.mapping.select_prev_item(),
+                    ["<C-j>"] = cmp.mapping.select_next_item(),
+                    ["<Tab>"] = cmp.mapping.confirm({ select = true }),
                     ["<C-Space>"] = cmp.mapping.complete(),
                     ["<C-e>"] = cmp.mapping.abort(),
-                    ["<C-j>"] = cmp.mapping.scroll_docs(-4),
-                    ["<C-k>"] = cmp.mapping.scroll_docs(4),
+                    ["<S-k>"] = cmp.mapping.scroll_docs(-4),
+                    ["<S-j>"] = cmp.mapping.scroll_docs(4),
                 }),
                 sources = cmp.config.sources({
                     { name = "luasnip" },
