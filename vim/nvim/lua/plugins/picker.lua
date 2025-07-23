c1b48cd1c8708ccd27fd21db6796f6d08e84eeea
uyaaaaaa <tosuyasocial48@gmail.com>
2025-07-24 08:12:28 +0900

    mod: picker keymap

--- vim/nvim/lua/plugins/picker.lua
+++ vim/nvim/lua/plugins/picker.lua
@@ -97,10 +97,10 @@ return {
                     input = {
                         keys = {
                             ["<Esc>"] = { "close", mode = { "n", "i" } },
-                            ["J"] = { "preview_scroll_down", mode = { "i", "n" } },
-                            ["K"] = { "preview_scroll_up", mode = { "i", "n" } },
-                            ["H"] = { "preview_scroll_left", mode = { "i", "n" } },
-                            ["L"] = { "preview_scroll_right", mode = { "i", "n" } },
+                            ["<C-j>"] = { "preview_scroll_down", mode = { "i", "n" } },
+                            ["<C-k>"] = { "preview_scroll_up", mode = { "i", "n" } },
+                            ["<C-h>"] = { "preview_scroll_left", mode = { "i", "n" } },
+                            ["<C-l>"] = { "preview_scroll_right", mode = { "i", "n" } },
                         },
                     },
                 },
