416c571a95f2f1475d374f7f599dff07985f4c69
uyaaaaaa <tosuyasocial48@gmail.com>
2025-07-26 18:36:06 +0900

    update: config

--- vim/nvim/lua/plugins/colorscheme.lua
+++ vim/nvim/lua/plugins/colorscheme.lua
@@ -3,42 +3,47 @@ return {
     {
         "cdmill/neomodern.nvim",
         lazy = true,
-        opts = {
-            theme = "iceclimber",
-            transparent = true,
-        },
+        config = function ()
+           require("neomodern").setup({
+                theme = "iceclimber",
+                transparent = true,
+            })
+            -- vim.cmd.colorscheme("iceclimber")
+        end,
     },
 
     -- night-owl
     {
         "oxfist/night-owl.nvim",
         lazy = true,
-        opts = {
-            itaric = false,
-            transparent_background = true,
-        },
+        config = function ()
+           require("night-owl").setup({
+                itaric = false,
+                transparent_background = true,
+            })
+            -- vim.cmd.colorscheme("night-owl")
+        end,
     },
 
     -- nightfox
     {
         -- nightfox | nordfox | carbonfox | duskfox
         "EdenEast/nightfox.nvim",
-        lazy = false,
+        lazy = true,
         priority = 1000,
-        opts = {
-            options = {
-                transparent = true,
-                styles = {
-                    comments = "italic",
-                    conditionals = "italic",
-                    functions = "italic",
-                    keywords = "italic",
+        config = function ()
+           require("nightfox").setup({
+                options = {
+                    transparent = true,
+                    styles = {
+                        comments = "italic",
+                        conditionals = "italic",
+                        functions = "italic",
+                        keywords = "italic",
+                    },
                 },
-            },
-        },
-        config = function(_, opts)
-            require("nightfox").setup(opts)
-            vim.cmd.colorscheme("duskfox")
+            })
+            -- vim.cmd.colorscheme("duskfox")
         end,
     },
 
@@ -46,10 +51,15 @@ return {
     {
         -- dark | darker | cool | deep | warm | warmer
         "navarasu/onedark.nvim",
+        lazy = false,
+        config = function ()
+            require("onedark").setup({
+                style = "darker",
+                transparent = true,
+            })
+            vim.cmd.colorscheme("onedark")
+        end,
+    },
         lazy = true,
-        opts = {
-            style = "darker",
-            transparent = true,
-        },
     },
 }
