f5cd9644225649bf43863bb39132600fbf46d7b1
uyaaaaaa <tosuyasocial48@gmail.com>
2025-07-28 08:00:54 +0900

    add: toggle csv function

--- vim/nvim/lua/utils/fn.lua
+++ vim/nvim/lua/utils/fn.lua
@@ -20,4 +20,30 @@ function M.ToggleVirtualText()
     vim.notify("Diagnostic virtual text: " .. (diagnostic_config.virtual_text and "enabled" or "disabled"), vim.log.levels.INFO)
 end
 
+function M.ToggleCsvFormat()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+    -- remove blank lines
+    local non_empty_lines = vim.tbl_filter(function(line)
+        return line:match("%S")
+    end, lines)
+
+    -- split to vertical if only camma separated line
+    if #non_empty_lines == 1 and string.match(non_empty_lines[1], "([^,]+)") then
+        local parts = vim.split(non_empty_lines[1], ",%s*")
+        vim.api.nvim_buf_set_lines(0, 0, -1, false, parts)
+        return
+    end
+
+    -- marge to camma separated line if not has camma
+    for _, line in ipairs(non_empty_lines) do
+        if line:find(",") then
+            return
+        end
+    end
+
+    local marged = table.concat(non_empty_lines, ", ")
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, {marged})
+end
+
 return M
