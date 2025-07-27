8977a83a0bff94dae7b71304de24f7b6adb8d4fd
uyaaaaaa <tosuyasocial48@gmail.com>
2025-07-27 10:41:32 +0900

    add: live-command

--- vim/nvim/lua/plugins/coding.lua
+++ vim/nvim/lua/plugins/coding.lua
@@ -61,4 +61,18 @@ return {
             require("illuminate").configure(opts)
         end,
     },
+
+    {
+        "smjonas/live-command.nvim",
+        event = { "CmdlineEnter" },
+        opts = {
+            commands = {
+                Norm = { cmd = "norm" },
+            },
+        },
+        config = function(_, opts)
+            require("live-command").setup(opts)
+            vim.cmd("cnoreabbrev norm Norm")
+        end,
+    },
 }
