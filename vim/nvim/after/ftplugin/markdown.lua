4a4b62559c65e40d8db7fbc6e7318fc730d4ccbf
uyaaaaaa <tosuyasocial48@gmail.com>
2025-08-11 18:16:14 +0900

    add link keymap

--- vim/nvim/after/ftplugin/markdown.lua
+++ vim/nvim/after/ftplugin/markdown.lua
@@ -1,3 +1,5 @@
 -- Set treesitter query
 vim.treesitter.query.set("markdown", "folds", "([(fenced_code_block) (indented_code_block) (section)] @fold (#trim! @fold))")
 
+vim.api.nvim_set_keymap("i", "<D-k>", "[]()<Left><Left><Left>", { noremap = true, silent = true })
+
