4c144efee0bb7264b08f743fcfe4cd696c9a3fa8
uyaaaaaa <tosuyasocial48@gmail.com>
2025-10-25 16:36:11 +0900

    add: custom command

--- vim/nvim/lua/command.lua
+++ vim/nvim/lua/command.lua
@@ -12,5 +12,14 @@ local function edit_snippet()
     vim.cmd("edit " .. snippet_path)
 end
 
+local function changeIndent(args)
+    local indent = tonumber(args)
+
+    vim.bo.shiftwidth = indent
+    vim.bo.softtabstop = indent
+    vim.bo.tabstop = indent
+end
+
 command("SnipEdit", edit_snippet, { desc = "Edit snippet" })
-command("Fm", function() vim.lsp.buf.format() end, { desc = "Save with format" })
+command("Fm", function() vim.lsp.buf.format() end, { desc = "Format" })
+command("Indent", function(opts) changeIndent(opts.args) end, { nargs = 1, desc = "Change indent" })
