f1b834b2c76e93033dceb141af66f3a2b306cae8
uyaaaaaa <tosuyasocial48@gmail.com>
2025-10-28 09:02:51 +0900

    fix: copilot key

--- vim/nvim/lua/plugins/agent.lua
+++ vim/nvim/lua/plugins/agent.lua
@@ -13,7 +13,7 @@ return {
                 enabled = true,
                 auto_trigger = true,
                 keymap = {
-                    accept = "<Tab>",
+                    accept = "<C-CR>",
                     accept_line = "<C-l>",
                     next = "<C-]>",
                     prev = "<C-[>",
