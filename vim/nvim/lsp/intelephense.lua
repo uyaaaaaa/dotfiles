49f06ed2d35932b1cdf510c0dbd7a829f9c54f9a
uyaaaaaa <tosuyasocial48@gmail.com>
2025-11-02 12:15:52 +0900

    update: intelephense

--- vim/nvim/lsp/intelephense.lua
+++ vim/nvim/lsp/intelephense.lua
@@ -5,9 +5,18 @@ return {
     settings = {
         intelephense = {
             files = {
+                maxSize = 5000000,
                 associations = {
                     "*.php",
                 },
+                exclude = {
+                    "./storage",
+                    "./bootstrap/cache",
+                    "./node_modules",
+                    "./vendor/**/tests",
+                },
+            },
+            environment = {
                 includePaths = {
                     "./_ide_helper.php",
                     "./_ide_helper_models.php",
