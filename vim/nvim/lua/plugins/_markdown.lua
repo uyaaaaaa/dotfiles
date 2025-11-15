c23efa14a903a8ac28a4f4e58ac15cf79957fc74
uyaaaaaa <tosuyasocial48@gmail.com>
2025-11-15 16:01:56 +0900

    fix

---
+++ vim/nvim/lua/plugins/_markdown.lua
@@ -0,0 +1,76 @@
+return {
+    -- toggle
+    {
+        "roodolv/markdown-toggle.nvim",
+        ft = { "markdown" },
+        opts = {
+            box_table = { "x" },
+            list_table = { "*" },
+        },
+        config = function()
+            vim.keymap.set("n", "<D-l>", require("markdown-toggle").checkbox_dot, { silent = true, noremap = true, expr = true })
+            vim.keymap.set("v", "<D-l>", require("markdown-toggle").checkbox, { silent = true, noremap = true, expr = false })
+        end,
+    },
+
+    -- bullets
+    {
+        "bullets-vim/bullets.vim",
+        ft = { "markdown" },
+    },
+
+    -- render
+    {
+        "MeanderingProgrammer/render-markdown.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "nvim-treesitter/nvim-treesitter",
+        },
+        ft = { "markdown", "mdx" },
+        opts = {
+            completions = { blink = { enabled = true } },
+            render_modes = { "n", "no", "c", "t", "v", "V", "i" },
+            link = {
+                email = ' ',
+                custom = {
+                    linkedin = { pattern = "%linkedin%.com", icon = " " },
+                },
+            },
+            dash = { width = 80 },
+            heading = {
+                width = "block",
+                left_pad = 0,
+                right_pad = 1,
+                icons = {},
+            },
+            quote = { repeat_linebreak = true },
+            code = {
+                sign = false,
+                width = "block",
+                border = "thick",
+                left_pad = 2,
+                right_pad = 2,
+                min_width = 50,
+                disable_background = {},
+            },
+            checkbox = {
+                checked = {
+                    icon = ' ',
+                    scope_highlight = "@markup.strikethrough",
+                },
+                unchecked = {
+                    icon = ' ',
+                    highlight = 'RenderMarkdownChecked',
+                },
+                custom = {
+                    todo = { raw = "", rendered = "", highlight = "" },
+                    canceled = {
+                        raw = "[-]",
+                        rendered = " ",
+                        scope_highlight = "@markup.strikethrough",
+                    },
+                },
+            },
+        },
+    }
+}
