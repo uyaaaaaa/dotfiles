72a9bd8b32a68d067db475f808d2c235d49aefe7
uyaaaaaa <tosuyasocial48@gmail.com>
2026-02-23 18:25:09 +0900

    update: keybind

--- wezterm/keybinds.lua
+++ wezterm/keybinds.lua
@@ -5,8 +5,10 @@ return {
   keys = {
     { key = "d", mods = "SUPER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
     { key = "d", mods = "SHIFT|SUPER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { key = "w", mods = "SUPER", action = act({ CloseCurrentPane = { confirm = true } }) },
+    { key = "w", mods = "SUPER", action = act({ CloseCurrentPane = { confirm = false } }) },
     { key = "[", mods = "SUPER", action = act.ActivatePaneDirection("Prev") },
     { key = "]", mods = "SUPER", action = act.ActivatePaneDirection("Next") },
+    { key = "p", mods = "SUPER", action = act.ActivateCommandPalette },
+    { key = "Enter", mods = "SUPER", action = act.ToggleFullScreen },
   }
 }
