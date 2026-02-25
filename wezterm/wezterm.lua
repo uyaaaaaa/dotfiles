3ed7b08ad11932511cddf000793af1c98fb048f3
uyaaaaaa <tosuyasocial48@gmail.com>
2026-02-26 08:46:10 +0900

    mod: icon color

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -57,13 +57,13 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
     ["vim"] = { icon = wezterm.nerdfonts.custom_vim, color = "#6666CC" },
     ["node"] = { icon = wezterm.nerdfonts.fa_node_js, color = "#84BA64" },
     ["python"] = { icon = wezterm.nerdfonts.fa_python, color = "#75A8D3" },
-    ["zsh"] = { icon = wezterm.nerdfonts.dev_terminal, color = "#8733F5" },
-    ["bash"] = { icon = wezterm.nerdfonts.dev_terminal, color = "#8733F5" },
-    ["git"] = { icon = wezterm.nerdfonts.fa_git_alt, color = "#8733F5" },
-    ["lazygit"] = { icon = wezterm.nerdfonts.fa_git_alt, color = "#8733F5" },
-    ["docker"] = { icon = wezterm.nerdfonts.fa_docker, color = "#8733F5" },
-    ["claude"] = { icon = wezterm.nerdfonts.fa_robot, color = "#8733F5" },
-    ["gemini"] = { icon = wezterm.nerdfonts.fa_robot, color = "#8733F5" },
+    ["zsh"] = { icon = wezterm.nerdfonts.dev_terminal, color = "#4D5156" },
+    ["bash"] = { icon = wezterm.nerdfonts.dev_terminal, color = "#4D5156" },
+    ["git"] = { icon = wezterm.nerdfonts.fa_git_alt, color = "#D7834F" },
+    ["lazygit"] = { icon = wezterm.nerdfonts.fa_git_alt, color = "#D7834F" },
+    ["docker"] = { icon = wezterm.nerdfonts.fa_docker, color = "#2560FF" },
+    ["claude"] = { icon = wezterm.nerdfonts.fa_robot, color = "#B46102" },
+    ["gemini"] = { icon = wezterm.nerdfonts.fa_robot, color = "#2560FF" },
   }
   local raw_process_name = tab.active_pane.foreground_process_name:gsub("(.*[/\\])(.*)", "%2")
   local process = processes[raw_process_name]
